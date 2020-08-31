3b8636632d2ca7f703eb4b39fcdb9de3c25c325b
Vitaliy Arkhanhelski <vit667@gmail.com>
2020-08-31 10:08:31 +0200

    initial commit

---
+++ src/main/java/pl/akademiakodu/kwejk/KwejkApplication.java
@@ -0,0 +1,13 @@
+package pl.akademiakodu.kwejk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwejkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwejkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/akademiakodu/kwejk/KwejkApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akademiakodu.kwejk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwejkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
