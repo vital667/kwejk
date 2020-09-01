ff4468dab0100a4678b453dc3869d9a4eed9d26a
Vitaliy Arkhanhelski <vit667@gmail.com>
2020-09-01 14:10:58 +0200

    added searching, return home if no result

--- src/main/java/pl/akademiakodu/kwejk/controller/GifController.java
+++ src/main/java/pl/akademiakodu/kwejk/controller/GifController.java
@@ -3,11 +3,12 @@ package pl.akademiakodu.kwejk.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import pl.akademiakodu.kwejk.entity.Gif;
 import pl.akademiakodu.kwejk.repo.GifRepository;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -25,7 +26,7 @@ public class GifController {
     @GetMapping("/gif/{name}")
     public String getGif(@PathVariable String name,
                          ModelMap map) {
-    //____________
+        //____________
 //        for (Gif gif : Gif.GIFS)
 //            if (gif.getName().equals(name))
 //                map.put("gif", gif);
@@ -40,6 +41,48 @@ public class GifController {
         map.put("gif", ((List<Gif>) gifRepository.findAll()).stream().
                 filter(g -> g.getName().equals(name)).findFirst().get());
         return "gif-details";
+    }
+
+
+    // @RequestMapping(value = "/getHome", method = RequestMethod.GET)
+    //@RequestMapping(value = "/gifs/search", method = RequestMethod.POST)
+    @GetMapping("/gifs/search")
+    @PostMapping(value = "/gifs/search")
+    public String searchGifs(@RequestParam String name,
+                             ModelMap map) {
+        List<Gif> gifs = new ArrayList<>();
+
+        for (Gif gif : gifRepository.findAll())
+            if (gif.getName().contains(name)) {
+                gifs.add(gif);
+            }
+
+        if (!gifs.isEmpty()) {
+            map.put("gifs", gifs);
+            return "home";
+        } else return "redirect:/";
+        // System.out.println(gifs);
+
+        //Iterable<Gif> iterable;
+//        List<Gif>list=new ArrayList<>();
+//        iterable.forEach(list::add);
+
+//        System.out.println(    )
+
+//        for (Gif gif : gifRepository.findAll())
+//            if (gif.getName().equals(name)) {
+//                map.put("gif", gif);
+//                return "gif-details";
+//            } else {
+//                return "home";
+//            }
+
+//        map.put("gif", ((List<Gif>) gifRepository.findAll()).stream().
+//                filter(g -> g.getName().equals(name)).findFirst().get());
+        // return "redirect:/";
+        //return "home";
+        // return "home";
+
     }
 
 
